44160f954f2c8090906db49117f2f2ec90f2c603
William R. Stetar <kebekad673@proton.me>
2024-10-07 19:23:13 -0500

    File Menu and Edit Menu Filled Out

--- src/editor.py
+++ src/editor.py
@@ -11,4 +11,14 @@ class Editor:
         if filename:
             with open(filename, 'r') as file:
                 self.textEdit.setText(file.read())
-        # Implement saveFile, newFile, etc.
+    def saveFile(self):
+        filename, _ = QFileDialog.getSaveFileName(self.parent, 'Save File', '', 'Text Files (*.txt);;All Files (*)')
+        if filename:
+            with open(filename, 'w') as file:
+                file.write(self.textEdit.toPlainText())  # Write the plain text from QTextEdit to the file
+    def saveFileAs(self):
+        self.saveFile()  # Just call the saveFile method
+
+    def newFile(self):
+        self.textEdit.clear()
+        
